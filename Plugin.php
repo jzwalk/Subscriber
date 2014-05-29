7062841dc748e77475f0e688e9f8cc897177d764
zyzzc <zyz615@gmail.com>
2014-05-29 09:57:47 +0800

    format fix

--- Plugin.php
+++ Plugin.php
@@ -2,7 +2,7 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 /**
- * 配合自定义字段功能实现指定内容仅注册会员可读。
+ * 配合自定义字段功能实现指定内容仅注册会员可读. 
  * 
  * @package Subscriber
  * @author 羽中
@@ -33,7 +33,7 @@ class Subscriber_Plugin implements Typecho_Plugin_Interface
 	 * @return void
 	 * @throws Typecho_Plugin_Exception
 	 */
-	public static function deactivate(){}
+	public static function deactivate() {}
 
 	/**
 	 * 获取插件配置面板
@@ -52,13 +52,13 @@ class Subscriber_Plugin implements Typecho_Plugin_Interface
 		self::subposts(),array(),_t('←会员文章列表'),_t(''));
 		$form->addInput($subpost->multiMode()->setAttribute('style','float:right;width:300px;'));
 		
-		$submsgp = new Typecho_Widget_Helper_Form_Element_Textarea('submsgp',NULL,'<div class="sub2view"><i class="icon-lock"></i>本文仅对注册会员开放阅读。</div>',_t('会员文章隐藏提示'),_t('未登录访客看到的会员文章显示，可使用html鼓励注册'));
+		$submsgp = new Typecho_Widget_Helper_Form_Element_Textarea('submsgp',NULL,'<div class="sub2view"><i class="icon-lock"></i>本文仅对注册会员开放阅读. </div>',_t('会员文章隐藏提示'),_t('未登录访客看到的会员文章显示, 可使用html鼓励注册'));
 		$form->addInput($submsgp->setAttribute('style','clear:both;'));
 		
-		$submsga1 = new Typecho_Widget_Helper_Form_Element_Textarea('submsga1',NULL,'<div class="sub2view"><i class="icon-lock"></i>此处内容需要登录才能查看。</div>',_t('会员内容隐藏提示'),_t('公开文章中用[sub][/sub]隐藏的会员内容对访客的提示'));
+		$submsga1 = new Typecho_Widget_Helper_Form_Element_Textarea('submsga1',NULL,'<div class="sub2view"><i class="icon-lock"></i>此处内容需要登录才能查看. </div>',_t('会员内容隐藏提示'),_t('公开文章中用[sub][/sub]隐藏的会员内容对访客的提示'));
 		$form->addInput($submsga1);
 		
-		$submsga2 = new Typecho_Widget_Helper_Form_Element_Textarea('submsga2',NULL,'<p>您已登录，可以阅读以下内容：</p><div class="sub2view"><i class="icon-lock-open"></i>{content}</div>',_t('会员内容可见效果'),_t('已登录会员看到隐藏内容显示效果(勿删改{content}标签)'));
+		$submsga2 = new Typecho_Widget_Helper_Form_Element_Textarea('submsga2',NULL,'<p>您已登录, 可以阅读以下内容：</p><div class="sub2view"><i class="icon-lock-open"></i>{content}</div>',_t('会员内容可见效果'),_t('已登录会员看到隐藏内容显示效果(勿删改{content}标签)'));
 		$form->addInput($submsga2);
 	}
 
@@ -69,7 +69,7 @@ class Subscriber_Plugin implements Typecho_Plugin_Interface
 	 * @param Typecho_Widget_Helper_Form $form
 	 * @return void
 	 */
-	public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+	public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
 
 	/**
 	 * 内容输出过滤
@@ -84,15 +84,15 @@ class Subscriber_Plugin implements Typecho_Plugin_Interface
 
 		$subcs = str_replace('{content}','$1',$option->submsga2);
 
-		$contents = ($widget->widget('Widget_User')->hasLogin())?
-					preg_replace("/\[sub\](.*?)\[\/sub\]/sm",''.$subcs.'',$content):
+		$contents = ($widget->widget('Widget_User')->hasLogin()) ?
+					preg_replace('/\[sub\](.*?)\[\/sub\]/sm',''.$subcs.'',$content):
 					''.$option->submsgp.'';
 
-		$content = isset($widget->fields->sub)?$contents:$content;
+		$content = isset($widget->fields->sub) ? $contents : $content;
 
-		$content = $widget->widget('Widget_User')->hasLogin()?
-			preg_replace("/\[sub\](.*?)\[\/sub\]/sm",''.$subcs.'',$content):
-			preg_replace("/\[sub\](.*?)\[\/sub\]/sm",''.$option->submsga1.'',$content);
+		$content = $widget->widget('Widget_User')->hasLogin() ?
+			preg_replace('/\[sub\](.*?)\[\/sub\]/sm',''.$subcs.'',$content):
+			preg_replace('/\[sub\](.*?)\[\/sub\]/sm',''.$option->submsga1.'',$content);
 
 		return $content;
 	}
@@ -116,7 +116,7 @@ class Subscriber_Plugin implements Typecho_Plugin_Interface
 
 		if (($option->subpost)) {
 			$db = Typecho_Db::get();
-			$cids = implode(",",$option->subpost);
+			$cids = implode(',',$option->subpost);
 			$db->query($db->delete('table.fields')->where('table.fields.cid in ('.$cids.')'));
 			}
 	}
@@ -134,11 +134,11 @@ class Subscriber_Plugin implements Typecho_Plugin_Interface
 		$subdata = $db->fetchAll($db
 		->select('table.contents.cid','table.contents.title')->from('table.contents')
 		->join('table.fields','table.fields.cid = table.contents.cid',Typecho_Db::INNER_JOIN)
-		->where('table.contents.type=?','post')
-		->where('table.fields.name=?','sub'));
+		->where('table.contents.type = ?','post')
+		->where('table.fields.name = ?','sub'));
 
 		$subposts = array();
-		foreach($subdata as $items){
+		foreach($subdata as $items) {
 			$subposts[$items['cid']]=($items['title']);
 		}
 
@@ -157,10 +157,10 @@ class Subscriber_Plugin implements Typecho_Plugin_Interface
 
 		$alldata = $db->fetchAll($db
 		->select('table.contents.cid','table.contents.title')->from('table.contents')
-		->where('table.contents.type=?','post'));
+		->where('table.contents.type = ?','post'));
 
 		$allposts = array();
-		foreach($alldata as $itema){
+		foreach($alldata as $itema) {
 			$allposts[$itema['cid']]=($itema['title']);
 		}
 
